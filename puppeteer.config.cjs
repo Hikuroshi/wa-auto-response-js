bd1f81aaded24ed96bffe8714f77d7bc68f3e62d
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-12-20 10:50:44 +0700

    update

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -7,6 +7,6 @@ module.exports = {
     cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
 
-const browser = await puppeteer.launch({
+await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
 });
