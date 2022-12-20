d906e8d051dfcbf3bc132de55ae4e3dcccc9f455
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-12-20 15:58:19 +0700

    update

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const qrcode = require('qrcode-terminal');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
-    authStrategy: new LocalAuth()
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ],
+        authStrategy: new LocalAuth()
+    }
 });
 
 client.on('qr', qr => {
